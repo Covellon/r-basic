85bbd00d74fad8a03d8e89d75f228a03cad8b937
Covellon <143110651+Covellon@users.noreply.github.com>
2023-09-13 13:59:35 -0300

    Update Operaciones con vectores lógicos.R

--- mis scripts/Operaciones con vectores lógicos.R
+++ mis scripts/Operaciones con vectores lógicos.R
@@ -20,7 +20,7 @@ any(vistas> 8000)
 
 all(vistas >2000)
 
-# Pero si preguntamos si todos los videos tien más de 5000 vistas nos responderá FALSE porque no todos las tienen, lo mismo que si pregunatamos si alguno tiene más de 10000, pq ninguno las tiene
+# Pero si preguntamos si todos los videos tienen más de 5000 vistas nos responderá FALSE porque no todos las tienen, lo mismo que si pregunatamos si alguno tiene más de 10000, pq ninguno las tiene
 
 all(vistas > 5000)
 all(vistas> 10000)
@@ -64,12 +64,98 @@ vistas[c("Video 2", "Video 4")]
 #Borré el vector que estaba mal de 5600 que estaba en la posición 9
 
 vistas[-9]
-
+---
 vistas[1]= 500
 vistas[c(2,3,4)]=c(600,700,800)
 vistas[vistas>7000]=7000
+vistas
 
-# --------------Vectores como Conjuntos-----------------------
+# --------------Utilizando los Vectores como Conjuntos-----------------------
+# Creamos 2 variables con los juegos que tienen 2 amigos (yo y amigo)
+
+yo=c("GTA V", "MORTAL COMBAT", "ZELDA","SKYRIM")
+
+amigo=c("MARIO KART", "ZELDA","DEVIL MAY CRAY","GTA V")
+
+yo
+amigo
+
+# Que videos tienen entre los dos? este no da bien.
+
+c(yo,amigo)
+union(yo, amigo)
+
+# Si queremos unirlos evitando repeticiones o sea que viedeos tenemos en común
+
+intersect(yo, amigo)
+
+# Que videos tengo yo que amigo no tiene?
+
+setdiff(yo, amigo)
+
+# Tenemos los mismos viedeo juegos? en consola me darà false pq no tenemos los mismos viedeo juegos
+
+setequal(yo,amigo)
+
+# si buscamos un dato particular, Ej.  queremos saber si amigo tiene el juego ZELDA. La respuesta en consola será TRUE
+
+"ZELDA"%in%amigo
+
+# Si queremos preguntar por varios podemos crear un vector.Dará False para el primero y True para el segundo
+
+c("AGE O EMPIRES", "MARIO KART")%in%amigo
+
+
+# --------------------Vectores Deterministas u otra manera de generar vectores----------------
+
+# Utilizando el operador : me creó 50 números correlativos, de 20 a 1 y de negativos a positivos
+
+1:50
+20:1
+-5:5
+
+# Utilizando la función seq puedo armar grupos por saltos
+
+seq(from = 0, to =50, by = 1)
+seq(from = 0, to = 50, by = 10)
+seq(from = 0, to = 50, by = 0.5)
+
+# Crear vectores a partir de carateres: 1) creo la variable juego, 2) le pido que me divida las palabras
+
+Juego= "MORTAL KOMBAT"
+split = strsplit(Juego, " ")[[1]]
+class(split)
+split
+
+# si lo quiero separar por cada uno de sus caracteres no dejo espacio entre las comillas y me devuelve cada letra de ambas palabras encomillada
+
+split = strsplit(Juego, "")[[1]]
+class(split)
+split
+
+# Buscamos si tiene una letra en particular, Ej si tien una "O" recordar que estaba en mayúscula, sin lo pongo en minúscula dará false
+
+any(split=="O")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
